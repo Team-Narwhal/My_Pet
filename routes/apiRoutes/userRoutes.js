a24a4509396f2934613fbee0077abd61ffc1b76a
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-30 15:40:33 -0700

    created routes for signin and signup

--- routes/apiRoutes/userRoutes.js
+++ routes/apiRoutes/userRoutes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-const { getAllUsers } = require('../../controllers/userController');
+const { getAllUsers, signUp, signIn } = require('../../controllers/userController');
+const { Pet } = require('../../models');
 
 
 // Routes for /api/user
+//renders signup/landing page
+
+//Asha
+//POST route for signin
+router.post('/signin', signIn);
+
+//route for /signup
+router.post('/signup', signUp);
+
 
 // Sample get route to /api/user to get all users
 router.route('/')
--- seeds/users.js
+++ seeds/users.js
@@ -4,12 +4,12 @@ const { User } = require('../models');
 const usersData = [
     {
         id: '8a923311-dd96-48d4-8817-f9705dfbee0e',
-        username: 'Becky',
+        email: 'becky@gmail.com',
         password: 'password',
     },
     {
         id: '8022d134-c225-494b-9845-41f594577e85',
-        username: 'Too Wong Fu',
+        email: 'cutepets@yahoo.com',
         password: 'password',
     },
 ];
