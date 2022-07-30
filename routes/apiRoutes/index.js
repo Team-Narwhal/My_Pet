a4ea2ad74269c0c04c09d941bb17eaf1b319ea3e
Angie <103882157+anakela@users.noreply.github.com>
2022-07-30 11:25:46 -1000

    Update index.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const petRoutes = require('./petRoutes');
 const userRoutes = require('./userRoutes');
 
-// Backend team yeag
 // Routes for pet
 router.use('/pet', petRoutes);
 // Routes for user
