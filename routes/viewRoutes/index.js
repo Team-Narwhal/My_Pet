d9dbd9d48a1e928e236f4243d55e9ac148c21355
howl-lab@github.com <howl-lab@github.com>
2022-07-30 17:35:26 -0700

    landing page's basic function working for sign up

--- routes/viewRoutes/index.js
+++ routes/viewRoutes/index.js
@@ -8,7 +8,7 @@ const router = require('express').Router();
 // Renders landing_page.handlebars
 router.get('/', (req, res) => {
     // Will add more here later.
-    res.render('/', {
+    res.render('landing_page', {
         isLoggedIn: req.session.isLoggedIn || false,
     });
 });
