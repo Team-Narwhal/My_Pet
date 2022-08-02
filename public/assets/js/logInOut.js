7ea9a30b1239ccfa6ea3dead9a9f3294581474c6
Nifer Kilakila <nifercritter@gmail.com>
2022-08-02 10:33:00 -0700

    Added a passwordHelp on signup.handlebars, started logInOut.js

---
+++ public/assets/js/logInOut.js
@@ -0,0 +1,21 @@
+const signinBtn = document.getElementById('signinBtn');
+const signInEmailInput = document.getElementById('signInEmailInput');
+const signInPasswordInput = document.getElementById('signInPasswordInput');
+
+signinBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const email = signInEmailInput.value;
+    const password = signInPasswordInput.value;
+
+    if
+
+
+    try {
+        
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+      }
+});
