825170f274a0497d9db4630d5c5c6f165401b311
Nifer Kilakila <nifercritter@gmail.com>
2022-08-02 10:58:56 -0700

    Commented out signUpNameInput in signup.handlebars to use in future development, added event listener for signinBtn in logInOut.js

--- public/assets/js/logInOut.js
+++ public/assets/js/logInOut.js
@@ -7,13 +7,29 @@ signinBtn.addEventListener('click', async (event) => {
     const email = signInEmailInput.value;
     const password = signInPasswordInput.value;
 
-    if
-
-
-    try {
-        
-
-
+        // checks to make sure username is not empty
+    if (email.trim().length === 0) {
+        alert('Please enter a valid email');
+        return;
+    }
+         // checks that password is greater than 6 characters
+    if (password.trim().length < 6) {
+        alert('Please enter a valid password.');
+        return;
+    }
+        try {
+        const response = await fetch('/api/user/signin', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            })
+        });
+        await response.json();
+        console.log(response);
     } catch (error) {
         console.log(error);
         res.status(500).json({ error });
