7c628da2f3b4b671cf03a7bb65c9e42fc7c7f935
Nifer Kilakila <nifercritter@gmail.com>
2022-08-03 11:48:45 -0700

    Added createPet.js in public, and began code

---
+++ public/assets/js/createPet.js
@@ -0,0 +1,6 @@
+const choosePetType = document.getElementById('choosePetType');
+const petNameInput = document.getElementById('petNameInput');
+const createPetBtn = document.getElementById('createPetBtn');
+
+
+
