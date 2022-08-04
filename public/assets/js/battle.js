2480e7917f58cf4dfac6bdd1e7f261dabbc183f0
ashachakre0906 <chourpagar.asha@gmail.com>
2022-08-04 15:53:07 -0700

    resolved the conflict

--- public/assets/js/battle.js
+++ public/assets/js/battle.js
@@ -1,33 +1,37 @@
-import { Battle } from '../lib/Battle.js';
+import { Battle } from "../lib/Battle.js";
 
 // Establish Socket Connection
 const socket = io();
-socket.emit('joined');
+socket.emit("joined");
 
 // example to build off of
-const yassBtn = document.getElementById('yass-btn');
-const ewBtn = document.getElementById('ew-btn');
+const yassBtn = document.getElementById("yass-btn");
+const ewBtn = document.getElementById("ew-btn");
 
-ewBtn.addEventListener('click', () => {
-    socket.emit('ew', 'ewwwwwww!');
+ewBtn.addEventListener("click", () => {
+  socket.emit("ew", "ewwwwwww!");
 });
 
-yassBtn.addEventListener('click', () => {
-    socket.emit('yass', 'YASSSSSSSSS!');
+yassBtn.addEventListener("click", () => {
+  socket.emit("yass", "YASSSSSSSSS!");
 });
 
-socket.on('ew', (data) => {
-    document.getElementById('socket-alert').textContent = data;
+socket.on("ew", (data) => {
+  document.getElementById("socket-alert").textContent = data;
 });
 
-socket.on('yass', (data) => {
-    document.getElementById('socket-alert').textContent = data;
+socket.on("yass", (data) => {
+  document.getElementById("socket-alert").textContent = data;
 });
 
 
 // Global Variables
 let myBattle;
 let myPet;
+//Asha
+//Write a function to fetch login user's active pet
+//inside the function we need to use the data to create the new instance of appropriate subclasses
+const getUserPet = async () => {};
 
 // Socketio Join another player's room
 // This is io.fetchSockets();
@@ -129,10 +133,10 @@ const initBattle = async () => {
 // Nolan
 // Function for Starts Battle
 const startBattle = () => {
-    // generate a new sequence
-    myBattle.generateSequence();
-    // display the sequence
-    displaySequence();
+  // generate a new sequence
+  myBattle.generateSequence();
+  // display the sequence
+  displaySequence();
 };
 
 // Nolan
@@ -140,16 +144,17 @@ const startBattle = () => {
 // Uses a setTimeout function to display the sequence to 
 // memorize to the player, arrows 'flash' one at a time
 const displaySequence = async () => {
-    // Get the sequence div to append to
-    const sequenceDiv = document.getElementById('sequence-div');
-    // Get the new sequence from the Battle object
-    const sequence = myBattle.sequence;
-    // Timeout function to clear the sequenceDiv
-    const clearDiv = async () => await new Promise(resolve => {
-        setTimeout(() => {
-            sequenceDiv.innerHTML = '';
-            setTimeout(() => resolve(), 800);
-        }, 1000);
+  // Get the sequence div to append to
+  const sequenceDiv = document.getElementById("sequence-div");
+  // Get the new sequence from the Battle object
+  const sequence = myBattle.sequence;
+  // Timeout function to clear the sequenceDiv
+  const clearDiv = async () =>
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        sequenceDiv.innerHTML = "";
+        setTimeout(() => resolve(), 800);
+      }, 1000);
     });
     // Loop through the sequence and display to user
     for (const direction of sequence) {
@@ -180,7 +185,6 @@ const defend = (success) => {
 
 initBattle();
 
-
 // generate a random room id if no rooms open
 // Create a game Class instance
 // Figure out socket rooms and connect to a game
@@ -189,18 +193,30 @@ initBattle();
 //Load the User's pet from database
 // Create a JS class instance
 //
-const petCanvas = document.getElementById('petCanvas');
-const battleButtons = document.querySelectorAll('.battle-button')
-//Initialize the function which will start the same 
-//Add Event listeners to the buttons
+const petCanvas = document.getElementById("petCanvas");
+const battleButtons = document.querySelectorAll(".battle-button");
+//Initialize the function which will start the same
+//Add Event listeners to the buttons and grabbing the value of the buttons
 console.log(battleButtons);
-battleButtons.forEach(btn => {
-    console.log('loop');
-    btn.addEventListener( 'click',(event) => {
+battleButtons.forEach((btn) => {
+  console.log("loop");
+  btn.addEventListener("click", (event) => {
     const direction = event.target.dataset.description;
     console.log(direction);
-
-    })
+    myBattle.sequenceGuess.push(direction);
+    const check = myBattle.sequenceCheck();
+    if (check) {
+      //if the check is true handle the win case
+      //this player block opponents attack
+      defend(check);
+    } else if (check === "continue") {
+      return;
+      
+    } else {
+      defend(check);
+      //check if the check is false
+      //so the opponents attack this player.
+      //substract opponent attack value from this player's pet HP value
+    }
+  });
 });
-
-
