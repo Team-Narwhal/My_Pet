d7a0b99b35222c9f461c95f94e7599e9b41e143b
Unicorn Barf <nolan.spence7@gmail.com>
2022-08-06 20:32:27 -0700

    Implemented a conversation feature to make the game more dynamic.

--- public/assets/js/battle.js
+++ public/assets/js/battle.js
@@ -4,6 +4,7 @@ let myPet;
 let enemyPet;
 let room;
 
+
 //Asha
 //Write a function to fetch login user's active pet
 //inside the function we need to use the data to create the new instance of appropriate subclasses
@@ -28,65 +29,93 @@ const socket = io();
 // Function for page initialization
 const startDiv = document.getElementById('start-div');
 const init = async () => {
-    // Call the function to get current user's pet
+  // Call the function to get current user's pet
 
-    // Creates a new Battle class instance
-    myBattle = new Battle;
-    // show start button once everything is loaded
-    await getUserPet ();
-    myPet.maxHp = myPet.hp
-    let maxHp = document.getElementById('my-hp')
-    maxHp.setAttribute ('max', myPet.hp);
-    updateHealthBar();
-    console.log(enemyPet);
-    //need to get the pet
-    // myHp.setAttribute('progress',);
-    startDiv.style.display = 'block';
+  // Creates a new Battle class instance
+  myBattle = new Battle;
+  // show start button once everything is loaded
+  await getUserPet();
+  myPet.maxHp = myPet.hp
+  let maxHp = document.getElementById('my-hp')
+  maxHp.setAttribute('max', myPet.hp);
+  updateHealthBar();
+  console.log(enemyPet);
+  //need to get the pet
+  // myHp.setAttribute('progress',);
+  startDiv.style.display = 'block';
 };
 //Calculation function/
 
- const updateHealthBar = async () => {
+const updateHealthBar = async () => {
   let myHp = document.getElementById('my-hp')
-   myHp.setAttribute ('value', myPet.hp);
-    myHp.textContent = `${myPet.hp/myPet.maxHp}%`;
-    console.log(myPet.hp);
-    console.log(enemyPet)
-    if (enemyPet){
-      let enemyhp = document.getElementById('enemy-hp')
-      enemyhp.setAttribute ('value', enemyPet.hp);
-      enemyhp.textContent = `${enemyPet.hp/enemyPet.maxHp}%`;
-    }
+  myHp.setAttribute('value', myPet.hp);
+  myHp.textContent = `${myPet.hp / myPet.maxHp}%`;
+  console.log(myPet.hp);
+  console.log(enemyPet)
+  if (enemyPet) {
+    let enemyhp = document.getElementById('enemy-hp')
+    enemyhp.setAttribute('value', enemyPet.hp);
+    enemyhp.textContent = `${enemyPet.hp / enemyPet.maxHp}%`;
+  }
 
 };
 //After each turn compare the currenthealth to total health
 
+// Nolan
+// conversation Function to show cute messages
+// type param: 'greeting', 'noDefend', 'defend', 'attack'
+// who param: 'me', 'enemy'
+const conversation = (type, who) => {
+  const meTalk = document.getElementById('me-talk');
+  const enemyTalk = document.getElementById('enemy-talk');
+  const phrase = myBattle.talk(type);
+  if (who === 'me') {
+    meTalk.textContent = phrase;
+    meTalk.parentElement.style.display = 'block';
+    setTimeout(() => {
+      meTalk.parentElement.style.display = 'none'
+    }, 5000);
+  } else {
+    enemyTalk.textContent = phrase;
+    enemyTalk.parentElement.style.display = 'block';
+    setTimeout(() => {
+      enemyTalk.parentElement.style.display = 'none'
+    }, 5000);
+  };
+};
+
+
 socket.on('you-first', (roomId) => {
-    // emit you-second event to send to other user
-    // send the room Id, and this user's pet
-    socket.emit('you-second', roomId, myPet);
-    myBattle.room = roomId;
-    startBattle();
+  // emit you-second event to send to other user
+  // send the room Id, and this user's pet
+  socket.emit('you-second', roomId, myPet);
+  myBattle.room = roomId;
 });
 
 // If you-second, this gets enemyPet
 socket.on('you-second', (roomId, pet) => {
-    room = roomId;
-    enemyPet = pet;
-    enemyPet.maxHp = enemyPet.hp
-    myBattle.room = roomId;
-    socket.emit('transfer-pet', roomId, myPet);
-    let maxHp = document.getElementById('enemy-hp')
-    maxHp.setAttribute ('max', enemyPet.hp);
-    updateHealthBar();
+  room = roomId;
+  enemyPet = pet;
+  enemyPet.maxHp = enemyPet.hp
+  myBattle.room = roomId;
+  socket.emit('transfer-pet', roomId, myPet);
+  let maxHp = document.getElementById('enemy-hp')
+  maxHp.setAttribute('max', enemyPet.hp);
+  updateHealthBar();
+  conversation('greeting', 'me');
+  conversation('greeting', 'enemy');
 });
 
 // If you-first, this gets enemyPet
 socket.on('transfer-pet', (pet) => {
-    enemyPet = pet;
-    enemyPet.maxHp = enemyPet.hp
-    let maxHp = document.getElementById('enemy-hp')
-    maxHp.setAttribute ('max', enemyPet.hp);
-    updateHealthBar();
+  enemyPet = pet;
+  enemyPet.maxHp = enemyPet.hp
+  let maxHp = document.getElementById('enemy-hp')
+  maxHp.setAttribute('max', enemyPet.hp);
+  updateHealthBar();
+  conversation('greeting', 'me');
+  conversation('greeting', 'enemy');
+  startBattle();
 });
 
 // Asha
@@ -96,8 +125,9 @@ socket.on('transfer-pet', (pet) => {
 socket.on('defend', (enemyHp) => {
   enemyPet.hp = enemyHp;
   console.log(enemyHp);
-    startBattle();
-    updateHealthBar();
+  conversation('defend', 'enemy');
+  startBattle();
+  updateHealthBar();
 });
 
 // Asha
@@ -107,13 +137,15 @@ socket.on('defend', (enemyHp) => {
 socket.on('no-defend', (enemyHp) => {
   enemyPet.hp = enemyHp;
   console.log(enemyHp);
-    startBattle();
-    updateHealthBar();
+  conversation('noDefend', 'enemy');
+  startBattle();
+  updateHealthBar();
 });
 
 // Nolan
 // Function for Starts Battle
 const startBattle = () => {
+  conversation('attack', 'me');
   // generate a new sequence
   myBattle.generateSequence();
   // display the sequence
@@ -137,57 +169,60 @@ const displaySequence = async () => {
         setTimeout(() => resolve(), 800);
       }, 1000);
     });
-    // Loop through the sequence and display to user
-    for (const direction of sequence) {
-        // Create and append appropriate Arrow image for every
-        // direction in the sequence array one at a time
-        let arrowImg = document.createElement('img');
-        arrowImg.src = '/assets/images/battle/' + direction + 'Arrow.svg';
-        sequenceDiv.append(arrowImg);
-        // Call clearDiv() to clear div with setTimeout timing
-        await clearDiv();
-    };
-    // Show buttons to the user
-    const gammingBtnDiv = document.getElementById('gamingButtons');
-    gammingBtnDiv.style.display = 'block';
+  // Loop through the sequence and display to user
+  for (const direction of sequence) {
+    // Create and append appropriate Arrow image for every
+    // direction in the sequence array one at a time
+    let arrowImg = document.createElement('img');
+    arrowImg.src = '/assets/images/battle/' + direction + 'Arrow.svg';
+    sequenceDiv.append(arrowImg);
+    // Call clearDiv() to clear div with setTimeout timing
+    await clearDiv();
+  };
+  // Show buttons to the user
+  const gammingBtnDiv = document.getElementById('gamingButtons');
+  gammingBtnDiv.style.display = 'block';
 }
 
 // Function to emit success or fail to other socket user
 // Param is either true for guessed sequence or false for failed guess
 const defend = (success) => {
-    // Hide buttons from User
-    const gammingBtnDiv = document.getElementById('gamingButtons');
-    gammingBtnDiv.style.display = 'none';
-    if (success) { 
-        // emit a 'defend' using socket
-        //emit an event
-        //This is for the client being attack
-        console.log(myPet.hp);
-        if(enemyPet.attack > myPet.defense){
-          myPet.hp -= (enemyPet.attack - myPet.defense)
-        }
-        if (myPet.hp <= 0) {
-          //endBattle
-          //call endBattle ();
-          //endBattle emit;
-          console.log('endBattle');
-        }
-        else {
-          socket.emit('defend',myBattle.room,myPet.hp)
-
-        }
-        // console.log(myPet.hp);
-        socket.emit('defend',myBattle.room,myPet.hp)
-          // console.log('success')
-          updateHealthBar();
+  // Hide buttons from User
+  const gammingBtnDiv = document.getElementById('gamingButtons');
+  gammingBtnDiv.style.display = 'none';
+  if (success) {
+    // Display conversation bubbles
+    conversation('defend', 'me');
+    conversation('attack', 'enemy');
+    // emit a 'defend' using socket
+    //emit an event
+    //This is for the client being attack
+    console.log(myPet.hp);
+    if (enemyPet.attack > myPet.defense) {
+      myPet.hp -= (enemyPet.attack - myPet.defense)
+    }
+    if (myPet.hp <= 0) {
+      //endBattle
+      //call endBattle ();
+      //endBattle emit;
+      console.log('endBattle');
     }
     else {
-        // emit a 'no-defend' using socket
-        myPet.hp -= enemyPet.attack
-        socket.emit('no-defend',myBattle.room,myPet.hp)
-        updateHealthBar();
-        console.log('fail')
+      socket.emit('defend', myBattle.room, myPet.hp)
+      updateHealthBar();
     }
+
+  }
+  else {
+    // Display conversation bubbles
+    conversation('noDefend', 'me');
+    conversation('attack', 'enemy');
+    // emit a 'no-defend' using socket
+    myPet.hp -= enemyPet.attack
+    socket.emit('no-defend', myBattle.room, myPet.hp)
+    updateHealthBar();
+    console.log('fail')
+  }
 };
 
 //Asha
@@ -221,9 +256,9 @@ battleButtons.forEach((btn) => {
 // start button for the battle to emit joined event
 const startBtn = document.querySelector('.start-btn');
 startBtn.addEventListener('click', () => {
-    socket.emit('joined');
-    // Hide the start button
-    startDiv.style.display = 'none';
+  socket.emit('joined');
+  // Hide the start button
+  startDiv.style.display = 'none';
 });
 
 // Start the app initialization
--- public/assets/lib/Battle.js
+++ public/assets/lib/Battle.js
@@ -5,6 +5,47 @@ class Battle {
     this.sequenceGuess = [];
     this.turn = true;
     this.roomId = '';
+    this.phrases = {
+      greeting: [
+        'Welcome to your death day!',
+        "I don't think you can handle this!",
+        "Surprised you're not running yet.",
+      ],
+      noDefend: [
+        'OUCH!',
+        'AHHHHHH!',
+        'HELP ME!',
+      ],
+      defend: [
+        "Can't touch this!",
+        "Well that didn't hurt...",
+        "You might as well have given up!",
+      ],
+      attack: [
+        "This is gonna hurt!",
+        "WooooAAHHHHH!",
+        "Brace yourself, I going to get you!",
+      ]
+
+    }
+  }
+
+  // Function to generate random gameplay phrases
+  talk(type) {
+    // generate random number 0-2
+    const index = Math.floor(Math.random() * 3);
+    switch (type) {
+      case 'greeting':
+        return this.phrases.greeting[index];
+      case 'noDefend':
+        return this.phrases.noDefend[index];
+      case 'defend':
+        return this.phrases.defend[index];
+      case 'attack':
+        return this.phrases.attack[index];
+      default:
+        return this.phrases.greeting[index];
+    };
   }
 
   // Function to generate a random sequence of u, d, r, l
@@ -19,37 +60,37 @@ class Battle {
     this.sequence = sequence;
   }
 
-    // Function to generate a random sequence of u, d, r, l
-    // returns an array of direction letters
-    generateSequence(num = 4) {
-        const directions = ['u', 'd', 'r', 'l',];
-        let sequence = [];
-        for (let i = 0; i < num; i++) {
-            // pushes a random element from directions array
-            sequence.push(directions[Math.floor(Math.random() * 4)]);
-        };
-        this.sequence = sequence;
+  // Function to generate a random sequence of u, d, r, l
+  // returns an array of direction letters
+  generateSequence(num = 4) {
+    const directions = ['u', 'd', 'r', 'l',];
+    let sequence = [];
+    for (let i = 0; i < num; i++) {
+      // pushes a random element from directions array
+      sequence.push(directions[Math.floor(Math.random() * 4)]);
     };
+    this.sequence = sequence;
+  };
 
-    // Function to check the user's sequence guess
-    // Will return success either true/false
-    sequenceCheck() {
-        const answer = this.sequence;
-        const guess = this.sequenceGuess;
-        if (answer.length === guess.length) {
-            for (let i = 0; i < answer.length; i++) {
-                if (answer[i] === guess[i]) {
-                    continue;
-                }
-                else {
-                    return false;
-                };
-            };
-            this.sequenceGuess = [];
-            return true;
+  // Function to check the user's sequence guess
+  // Will return success either true/false
+  sequenceCheck() {
+    const answer = this.sequence;
+    const guess = this.sequenceGuess;
+    if (answer.length === guess.length) {
+      for (let i = 0; i < answer.length; i++) {
+        if (answer[i] === guess[i]) {
+          continue;
+        }
+        else {
+          return false;
         };
-        return 'continue';
+      };
+      this.sequenceGuess = [];
+      return true;
     };
+    return 'continue';
+  };
 
 
 }
